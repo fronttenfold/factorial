3ed0a1d33d1af90e2c0d44c415342fcb549d7beb
fronttenfold <138482272+fronttenfold@users.noreply.github.com>
2023-07-03 13:33:08 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# Prompt the user to enter a number
+num = int(input("Enter a number: "))
+
+# Calculate the factorial
+result = factorial(num)
+
+# Display the result
+print("The factorial of", num, "is", result)
+
